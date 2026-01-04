207e4c0efb1e5c850bf76fbf844f47cba5eb9d9a
Ali747711 <126062847+Ali747711@users.noreply.github.com>
2026-01-04 19:38:21 +0900

    New structure of frontend

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world</h1>
+      <button className="min-w-20 min-h-10 bg-main hover:bg-main-dull text-main-text cursor-pointer rounded  ">
+        Buttom
+      </button>
+      <a href="#">Google</a>
+    </>
+  );
+}
+
+export default App;
